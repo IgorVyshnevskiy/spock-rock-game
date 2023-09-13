009b8ff66cd9c249b55d9edff87edd49684cc64e
Igor Vyshnevskyi <igor.vyshnevskiy097@gmail.com>
2023-09-14 00:17:52 +0300

    finished base logic for game

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-const playerScoreEl = document.getElementById('playerScore')
-const playerChoiceEl = document.getElementById('playerChoice')
-const computerScoreEl = document.getElementById('computerScore')
-const computerChoiceEl = document.getElementById('computerChoice')
-const resultText = document.getElementById('resultText')
+const playerScoreEl = document.getElementById('playerScore');
+const playerChoiceEl = document.getElementById('playerChoice');
+const computerScoreEl = document.getElementById('computerScore');
+const computerChoiceEl = document.getElementById('computerChoice');
+const resultText = document.getElementById('resultText');
 
-const playerRock = document.getElementById('playerRock')
-const playerPaper = document.getElementById('playerPaper')
-const playerScissors = document.getElementById('playerScissors')
-const playerLizard = document.getElementById('playerLizard')
-const playerSpock = document.getElementById('playerSpock')
+const playerRock = document.getElementById('playerRock');
+const playerPaper = document.getElementById('playerPaper');
+const playerScissors = document.getElementById('playerScissors');
+const playerLizard = document.getElementById('playerLizard');
+const playerSpock = document.getElementById('playerSpock');
 
-const computerRock = document.getElementById('computerRock')
-const computerPaper = document.getElementById('computerPaper')
-const computerScissors = document.getElementById('computerScissors')
-const computerLizard = document.getElementById('computerLizard')
-const computerSpock = document.getElementById('computerSpock')
+const computerRock = document.getElementById('computerRock');
+const computerPaper = document.getElementById('computerPaper');
+const computerScissors = document.getElementById('computerScissors');
+const computerLizard = document.getElementById('computerLizard');
+const computerSpock = document.getElementById('computerSpock');
 
-const allGameIcons = document.querySelectorAll('.far')
+const allGameIcons = document.querySelectorAll('.far');
 
 const choices = {
   rock: { name: 'Rock', defeats: ['scissors', 'lizard'] },
@@ -26,92 +26,121 @@ const choices = {
   spock: { name: 'Spock', defeats: ['scissors', 'rock'] },
 };
 
-let computerChoice = ''
+let computerChoice = '';
+let computerScorenumber = 0;
+let playerScorenumber = 0;
 
 function resetSelected() {
   allGameIcons.forEach((icon) => {
-    icon.classList.remove('selected')
-  })
+    icon.classList.remove('selected');
+  });
+}
+
+function resetAll() {
+  playerScorenumber = 0;
+  computerScorenumber = 0;
+  playerScoreEl.textContent = playerScorenumber;
+  computerScoreEl.textContent = computerScorenumber;
+  playerChoiceEl.textContent = '';
+  computerChoiceEl.textContent = '';
+  resultText.textContent = ''
+  resetSelected()
 }
 
 function computerRandomChoice() {
-  const computerChoiceNumber = Math.random()
+  const computerChoiceNumber = Math.random();
   if (computerChoiceNumber < 0.2) {
-    computerChoice = 'rock'
+    computerChoice = 'rock';
   } else if (computerChoiceNumber <= 0.4) {
-    computerChoice = 'paper'
-  }
-   else if (computerChoiceNumber <= 0.6) {
-    computerChoice = 'scissors'
-  }
-   else if (computerChoiceNumber <= 0.8) {
-    computerChoice = 'lizard'
-  }
-   else  {
-    computerChoice = 'spock'
+    computerChoice = 'paper';
+  } else if (computerChoiceNumber <= 0.6) {
+    computerChoice = 'scissors';
+  } else if (computerChoiceNumber <= 0.8) {
+    computerChoice = 'lizard';
+  } else {
+    computerChoice = 'spock';
   }
 }
 
 function displayComputerChoice(playerChoice) {
   switch (computerChoice) {
     case 'rock':
-      computerRock.classList.add('selected')
-      computerChoiceEl.textContent = ' --- Rock'
+      computerRock.classList.add('selected');
+      computerChoiceEl.textContent = ' --- Rock';
       break;
     case 'paper':
-      computerPaper.classList.add('selected')
-      computerChoiceEl.textContent = ' --- Paper'
+      computerPaper.classList.add('selected');
+      computerChoiceEl.textContent = ' --- Paper';
       break;
     case 'scissors':
-      computerScissors.classList.add('selected')
-      computerChoiceEl.textContent = ' --- Scissors'
+      computerScissors.classList.add('selected');
+      computerChoiceEl.textContent = ' --- Scissors';
       break;
     case 'lizard':
-      computerLizard.classList.add('selected')
-      computerChoiceEl.textContent = ' --- Lizard'
+      computerLizard.classList.add('selected');
+      computerChoiceEl.textContent = ' --- Lizard';
       break;
     case 'spock':
-      computerSpock.classList.add('selected')
-      computerChoiceEl.textContent = ' --- Spock'
+      computerSpock.classList.add('selected');
+      computerChoiceEl.textContent = ' --- Spock';
       break;
-  
+
     default:
       break;
   }
 }
 
-function checkResult() {
-  resetSelected();
-  computerRandomChoice()
-  displayComputerChoice()
+function updateScore(playerChoice) {
+  if (playerChoice === computerChoice) {
+    resultText.textContent = "It's a tie.";
+  } else {
+    const choice = choices[playerChoice];
+    if (choice.defeats.indexOf(computerChoice) > -1) {
+      resultText.textContent = 'You Won!';
+      playerScorenumber += 1;
+      playerScoreEl.textContent = playerScorenumber;
+    } else {
+      resultText.textContent = 'You Lose!';
+      computerScorenumber += 1;
+      computerScoreEl.textContent = computerScorenumber;
+    }
+  }
+}
 
+function checkResult(playerChoice) {
+  resetSelected();
+  computerRandomChoice();
+  displayComputerChoice();
+  updateScore(playerChoice);
 }
 
 function select(playerChoice) {
-  checkResult()
+  checkResult(playerChoice);
   switch (playerChoice) {
     case 'rock':
-      playerRock.classList.add('selected')
-      playerChoiceEl.textContent = ' --- Rock'
+      playerRock.classList.add('selected');
+      playerChoiceEl.textContent = ' --- Rock';
       break;
     case 'paper':
-      playerPaper.classList.add('selected')
-      playerChoiceEl.textContent = ' --- Paper'
+      playerPaper.classList.add('selected');
+      playerChoiceEl.textContent = ' --- Paper';
       break;
     case 'scissors':
-      playerScissors.classList.add('selected')
-      playerChoiceEl.textContent = ' --- Scissors'
+      playerScissors.classList.add('selected');
+      playerChoiceEl.textContent = ' --- Scissors';
       break;
     case 'lizard':
-      playerLizard.classList.add('selected')
-      playerChoiceEl.textContent = ' --- Lizard'
+      playerLizard.classList.add('selected');
+      playerChoiceEl.textContent = ' --- Lizard';
       break;
     case 'spock':
-      playerSpock.classList.add('selected')
-      playerChoiceEl.textContent = ' --- Spock'
+      playerSpock.classList.add('selected');
+      playerChoiceEl.textContent = ' --- Spock';
       break;
-  
+
     default:
       break;
   }
 }
+
+resetAll()
